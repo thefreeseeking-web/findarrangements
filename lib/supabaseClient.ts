618c164b099963b3b29fe1bf1bf7cc42175161cf
thefreeseeking-web <thefreeseeking@gmail.com>
2026-07-26 18:26:09 -0800

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,7 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 // This client runs in the browser and automatically handles the logged-in
 // user's session (cookies) so every request is scoped to them.
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  return createClientComponentClient();
 }
